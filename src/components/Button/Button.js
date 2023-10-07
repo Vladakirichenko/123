06f6dc9259229d97d69a19012bc73c72dae1265e
Vlada Kirichenko <vlada.kirichenko@vlada.kirichenko>
2023-10-07 20:08:38 +0300

    add login logic

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ import './Button.css'
 const Button = props => {
     return (
         <div className="button-wrap">
-            <button className="btn">
+            <button className="btn" onClick={props.login}>
                 {props.name}
             </button>
         </div>
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import ProductsTable from './containers/ProductsTable/ProductsTable';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Login /> */}
-    <ProductsTable/>
+    <Login />
+    {/* <ProductsTable/> */}
   </React.StrictMode>
 );
